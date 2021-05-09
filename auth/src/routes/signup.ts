2927cef51fc5297c6093b8a1ab3cfa782243364d
JaiRaga <raga.j.santhosh@gmail.com>
2021-05-09 15:16:30 +0530

    implemnted req validation and db error checking

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 const router = express.Router()
 
@@ -16,7 +18,7 @@ router.post(
 		const errors = validationResult(req)
 
 		if (!errors.isEmpty()) {
-			throw new Error('Invalid Email or Password!')
+			throw new RequestValidationError(errors.array())
 			// return res.status(400).send(errors.array())
 		}
 
@@ -24,7 +26,7 @@ router.post(
 
 		console.log('Creating a user!!!!')
 
-		throw new Error('Cant connect to db!')
+		throw new DatabaseConnectionError()
 
 		res.send({})
 	}
