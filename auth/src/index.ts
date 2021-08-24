23a451e2ed7d4d04014044e1ad86e7ddc47c47d9
JaiRaga <raga.j.santhosh@gmail.com>
2021-08-24 15:25:02 +0530

    added cookie session as middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 import 'express-async-errors'
 import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signupRouter } from './routes/signup'
@@ -11,7 +13,14 @@ import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 
+app.set('trust proxy', true)
 app.use(json())
+app.use(
+	cookieSession({
+		signed: false,
+		secure: true,
+	})
+)
 
 app.use(currentUserRouter)
 app.use(signinRouter)
