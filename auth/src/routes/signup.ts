173fa9846bf378721bcb9ddccd71d4308e997f79
JaiRaga <raga.j.santhosh@gmail.com>
2021-08-24 15:59:07 +0530

    created jwt and assigned it to sessions object

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+
 import { body, validationResult } from 'express-validator'
 import { BadRequestError } from '../errors/bad-request-error'
 import { RequestValidationError } from '../errors/request-validation-error'
@@ -33,6 +35,20 @@ router.post(
 
 		const user = User.build({ email, password })
 		await user.save()
+
+		// Generate JWT
+		const userJwt = jwt.sign(
+			{
+				id: user.id,
+				email: user.email,
+			},
+			'asde'
+		)
+
+		// Store it on session object
+		req.session = {
+			jwt: userJwt,
+		}
 
 		res.status(201).send(user)
 	}
