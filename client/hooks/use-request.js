c0ac8fd9e13cf8d4b379aff2c247d09cab75454b
JaiRaga <raga.j.santhosh@gmail.com>
2021-09-14 21:19:55 +0530

    :hammer: removed console logs

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -7,7 +7,9 @@ export default ({ url, method, body, onSuccess }) => {
 	const doRequest = async () => {
 		try {
 			setErrors(null)
+
 			const response = await axios[method](url, body)
+
 			if (onSuccess) {
 				onSuccess(response.data)
 			}
